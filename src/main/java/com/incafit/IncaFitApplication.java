f1899def8942db74914848c5e9943587d58d97b3
Nikolas Medina <nikkmed805@gmail.com>
2025-07-11 18:33:23 +0200

    CONFIGURACIÓN

---
+++ src/main/java/com/incafit/IncaFitApplication.java
@@ -0,0 +1,13 @@
+package com.incafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncaFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncaFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incafit/IncaFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncaFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
